6f43bc58f88a7becd2de2a26c7e804afcf544704
Ben Brandt <bbrandt@users.noreply.github.com>
2017-07-12 21:25:56 -0500

    Initial state

---
+++ src/FizzBuzz/FizzBuzzGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzGenerator
+    {
+        public IEnumerable<string> GetNumbers(int start, int end)
+        {
+            foreach (var i in Enumerable.Range(start, end-start+1))
+            {
+                if (i % 15 == 0)
+                {
+                    yield return "fizzbuzz";
+                }
+                else if (i % 3 == 0)
+                {
+                    yield return "fizz";
+                }
+                else if (i % 5 == 0)
+                {
+                    yield return "buzz";
+                }
+                else
+                {
+                    yield return i.ToString();
+                }
+            }
+        }
+
+    }
+}
---
+++ src/FizzBuzzConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FizzBuzz;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var fizzBuzzer = new FizzBuzzGenerator();
+
+            foreach (var value in fizzBuzzer.GetNumbers(1, Int32.MaxValue))
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
